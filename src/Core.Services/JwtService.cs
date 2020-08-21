4ed0aa75b7253023c4d89c014fc06fccdca89355
niltor <zpty@outlook.com>
2020-08-21 14:05:14 +0800

    api 项目基础配置

---
+++ src/Core.Services/JwtService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Services
+{
+    public class JwtService
+    {
+        public JwtService()
+        {
+        }
+        /// <summary>
+        /// 生成jwt token
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="sign"></param>
+        /// <param name="audience"></param>
+        /// <param name="issuer"></param>
+        /// <returns></returns>
+        public string BuildToken(string username, string sign, string audience, string issuer)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(sign));
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            var claims = new Claim[]
+            {
+                // 此处自定义claims
+                new Claim(ClaimTypes.Name, username),
+            };
+            var jwt = new JwtSecurityToken(issuer, audience, claims, signingCredentials: signingCredentials);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            return encodedJwt;
+        }
+    }
+}
--- src/Host.Api/Startup.cs
+++ src/Host.Api/Startup.cs
@@ -1,10 +1,16 @@
 using AutoMapper;
+using Core.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Services.AutoMapper;
+using System.Security.Claims;
+using System.Text;
+
 namespace App.Api
 {
     public class Startup
@@ -21,32 +27,98 @@ namespace App.Api
 
             // 当使用GT.CLI生成仓储服务后，请取消下行注释
             // services.AddRepositories();
-
-            // automapper服务
             services.AddAutoMapper(typeof(MapperProfile));
             services.AddHttpContextAccessor();
 
+            #region 添加接口jwt授权、swagger、cors等服务
+            // 添加验证
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(cfg =>
+            {
+                //cfg.RequireHttpsMetadata = true;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    // 配置 jwt 验证，或使用identityServer
+                    //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("Jwt")["Sign"])),
+                    //ValidIssuer = Configuration.GetSection("Jwt")["Issuer"],
+                    //ValidAudience = Configuration.GetSection("Jwt")["Audience"],
+                    ValidateAudience = false,
+                    //ValidateIssuer = true,
+                    ValidateLifetime = false,
+                    RequireExpirationTime = false,
+                    //ValidateIssuerSigningKey = true
+                };
+            });
+            // 添加授权
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("User", policy => policy.RequireClaim(ClaimTypes.Name));
+            });
 
-            services.AddControllers();
-            //services.AddControllers()
-            //    .AddNewtonsoftJson(options =>
-            //    {
-            //        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            //    }); 
+            // services.AddScoped(typeof(JwtService)); 
+           // cors配置
+            services.AddCors(options =>
+            {
+                options.AddPolicy("default", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+            // swagger 配置
+            services.AddOpenApiDocument(doc =>
+            {
+                doc.PostProcess = post =>
+                {
+                    post.Info.Version = "v1";
+                    post.Info.Title = "网站接口";
+                    post.Info.Description = "网站接口";
+                    post.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Niltor",
+                        Email = string.Empty,
+                        Url = "https://github.com/geethin/"
+                    };
+                };
+                doc.DocumentName = "web";
+                doc.ApiGroupNames = new[] { "Web" };
+            });
+            #endregion
+
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseCors("default");
                 app.UseDeveloperExceptionPage();
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
-
-            app.UseHttpsRedirection();
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                //app.UseHsts();
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
